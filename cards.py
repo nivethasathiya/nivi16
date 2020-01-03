461b14aea2606bd523a89e644208237ab6baf4d8
nivethasathiya <53109733+nivethasathiya@users.noreply.github.com>
2020-01-03 12:02:43 +0530

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void swap(int*a,int*b)
+{
+  int temp,a,b;
+  scanf("%d %d", &a, &b);
+  printf("enter the possible numbers in playing cards:\n");
+         {
+           temp=*a;
+           *a=*b;
+           *b=temp;
+         }
+         printf("\n");
+         }
+         return 0;
+
+  
